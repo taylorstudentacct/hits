971ec4b2c26ee5741204b2ad6bd63b4f4efb3c71
anonymous anonymous <taylor.james@kellogg.northwestern.edu>
2021-12-07 18:19:42 +0000

    Whitelist columns in Rapid for ActiveAdmin

--- app/admin/rapids.rb
+++ app/admin/rapids.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Rapid do
 
+ permit_params :name, :river_id, :location, :description
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #

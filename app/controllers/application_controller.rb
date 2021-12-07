a6141ed038c3d59fca67c2cde4db05af044d8932
anonymous anonymous <taylor.james@kellogg.northwestern.edu>
2021-12-07 18:19:04 +0000

    Generate users resource with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :first_name, :last_name, :user_type, :phone_number])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:username, :first_name, :last_name, :user_type, :phone_number])
+  end
 end
